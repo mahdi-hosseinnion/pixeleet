dcc60ca6796acadce5814772c16cf719d47fbc44
mahdi <ssmmhh1382@gmail.com>
2022-06-28 22:06:17 +0430

    implement select image logic in SelectImageScreen

--- app/src/main/java/com/ssmmhh/pixeleet/presentation/select_image/SelectImageScreen.kt
+++ app/src/main/java/com/ssmmhh/pixeleet/presentation/select_image/SelectImageScreen.kt
@@ -1,18 +1,18 @@
 package com.ssmmhh.pixeleet.presentation.select_image
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import dagger.hilt.android.lifecycle.HiltViewModel
+
+private const val TAG = "SelectImageScreen"
 
 @Composable
 @Destination(start = true)
@@ -20,10 +20,24 @@ fun SelectImageScreen(
     navigator: DestinationsNavigator,
     viewModel: SelectImageViewModel = hiltViewModel()
 ) {
-    Text(
-        "Hey it is SelectImageScreen",
-        modifier = Modifier.fillMaxSize(),
-        fontSize = 24.sp,
-        color = MaterialTheme.colors.onBackground
-    )
+    val launcher = rememberLauncherForActivityResult(
+        contract =
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        viewModel.setImageUriTo(uri)
+    }
+    Column() {
+        Button(onClick = {
+            launcher.launch("image/*")
+        }) {
+            Text(text = "Pick image")
+        }
+
+        viewModel.imageUri.let {
+            //TODO("Navigate to next screen")
+            Log.d(TAG, "SelectImageScreen: called with $it")
+        }
+
+    }
+
 }
--- app/src/main/java/com/ssmmhh/pixeleet/presentation/select_image/SelectImageViewModel.kt
+++ app/src/main/java/com/ssmmhh/pixeleet/presentation/select_image/SelectImageViewModel.kt
@@ -1,5 +1,9 @@
 package com.ssmmhh.pixeleet.presentation.select_image
 
+import android.net.Uri
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,4 +14,11 @@ class SelectImageViewModel
 constructor(
 
 ) : ViewModel() {
+
+    var imageUri by mutableStateOf<Uri?>(null)
+
+    fun setImageUriTo(uri: Uri?) {
+        imageUri = uri
+    }
+
 }
