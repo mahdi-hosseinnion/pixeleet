1a7e4a9d9ec65bf66ce46203855d946ce6a5caeb
mahdi <ssmmhh1382@gmail.com>
2022-07-02 00:22:40 +0430

    convert image to ascii but with color

--- app/src/main/java/com/ssmmhh/pixeleet/presentation/show_image/ShowImageScreen.kt
+++ app/src/main/java/com/ssmmhh/pixeleet/presentation/show_image/ShowImageScreen.kt
@@ -1,18 +1,21 @@
 package com.ssmmhh.pixeleet.presentation.show_image
 
-import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.PixelMap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.toPixelMap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
+
+private const val TAG = "ShowImageScreen"
 
 @Composable
 @Destination
@@ -20,23 +23,24 @@ fun ShowImageScreen(
     imageUri: Uri,
     viewModel: ShowImageViewModel = hiltViewModel()
 ) {
-    var bitmap by remember {
-        mutableStateOf<Bitmap?>(null)
-    }
     val context = LocalContext.current
-    bitmap = if (Build.VERSION.SDK_INT < 28) {
-        MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
-    } else {
-        val source = ImageDecoder.createSource(context.contentResolver, imageUri)
-        ImageDecoder.decodeBitmap(source)
+    val pixelMap: PixelMap = remember {
+        if (Build.VERSION.SDK_INT < 28) {
+            MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
+                .asImageBitmap().toPixelMap()
+        } else {
+            val source = ImageDecoder.createSource(context.contentResolver, imageUri)
+            ImageDecoder.decodeBitmap(source)
+                .asImageBitmap().toPixelMap()
+        }
     }
+    viewModel.setPixelMapTo(pixelMap)
 
-    bitmap?.let { btm ->
-        Image(
-            bitmap = btm.asImageBitmap(),
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize()
-        )
-    }
+    Text(text = viewModel.textViewResult, fontSize = 2.sp)
+
 
 }
+
+
+
+
--- app/src/main/java/com/ssmmhh/pixeleet/presentation/show_image/ShowImageViewModel.kt
+++ app/src/main/java/com/ssmmhh/pixeleet/presentation/show_image/ShowImageViewModel.kt
@@ -1,7 +1,20 @@
 package com.ssmmhh.pixeleet.presentation.show_image
 
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.PixelMap
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -9,4 +22,51 @@ class ShowImageViewModel
 @Inject
 constructor(
 
-) : ViewModel() {}
+) : ViewModel() {
+    private val TAG = "ShowImageViewModel"
+
+    private var pixelMap: MutableStateFlow<PixelMap?> = MutableStateFlow(null)
+
+    var textViewResult by mutableStateOf(AnnotatedString("Loading..."))
+
+    fun setPixelMapTo(value: PixelMap) {
+        Log.d(TAG, "setPixelMapTo: called with x${value.height}y${value.width}")
+        if (pixelMap.value != null) return
+        pixelMap.value = value
+    }
+
+    init {
+        viewModelScope.launch {
+            pixelMap.collect {
+                it?.let { loadTheImage(it) }
+            }
+        }
+    }
+
+    private fun loadTheImage(pixelMap: PixelMap) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = AnnotatedString.Builder()
+            for (i in 0 until pixelMap.height) {
+                for (j in 0 until pixelMap.width) {
+                    val color = pixelMap[j, i]
+                    result.pushStyle(
+                        SpanStyle(
+                            color = color,
+                        )
+                    )
+                    result.append("@")
+                    result.pop()
+                }
+                result.append("\n")
+            }
+            withContext(
+                Dispatchers.Main
+            ) {
+                textViewResult = result.toAnnotatedString()
+            }
+
+        }
+    }
+
+
+}
