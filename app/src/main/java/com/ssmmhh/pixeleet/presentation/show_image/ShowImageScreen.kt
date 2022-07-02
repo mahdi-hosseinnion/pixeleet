a9235450e8087af56cef0ab093de5467d14a9688
mahdi <ssmmhh1382@gmail.com>
2022-07-02 23:36:10 +0430

    show basic ascii art

--- app/src/main/java/com/ssmmhh/pixeleet/presentation/show_image/ShowImageScreen.kt
+++ app/src/main/java/com/ssmmhh/pixeleet/presentation/show_image/ShowImageScreen.kt
@@ -7,10 +7,11 @@ import android.provider.MediaStore
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.PixelMap
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.toPixelMap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily.Companion.Monospace
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
@@ -24,20 +25,23 @@ fun ShowImageScreen(
     viewModel: ShowImageViewModel = hiltViewModel()
 ) {
     val context = LocalContext.current
-    val pixelMap: PixelMap = remember {
-        if (Build.VERSION.SDK_INT < 28) {
+    remember {
+        val bitmap = if (Build.VERSION.SDK_INT < 28) {
             MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
-                .asImageBitmap().toPixelMap()
         } else {
             val source = ImageDecoder.createSource(context.contentResolver, imageUri)
             ImageDecoder.decodeBitmap(source)
-                .asImageBitmap().toPixelMap()
         }
+        viewModel.setPixelMapTo(bitmap.asImageBitmap().toPixelMap())
+        bitmap
     }
-    viewModel.setPixelMapTo(pixelMap)
 
-    Text(text = viewModel.textViewResult, fontSize = 2.sp)
-
+    Text(
+        text = viewModel.textViewResult,
+        color = Color.Black,
+        fontSize = 1.5.sp,
+        fontFamily = Monospace
+    )
 
 }
 
--- app/src/main/java/com/ssmmhh/pixeleet/presentation/show_image/ShowImageViewModel.kt
+++ app/src/main/java/com/ssmmhh/pixeleet/presentation/show_image/ShowImageViewModel.kt
@@ -4,9 +4,8 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PixelMap
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,8 +14,10 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.lang.StringBuilder
 import javax.inject.Inject
 
+//Important source
 @HiltViewModel
 class ShowImageViewModel
 @Inject
@@ -27,10 +28,9 @@ constructor(
 
     private var pixelMap: MutableStateFlow<PixelMap?> = MutableStateFlow(null)
 
-    var textViewResult by mutableStateOf(AnnotatedString("Loading..."))
+    var textViewResult by mutableStateOf("")
 
     fun setPixelMapTo(value: PixelMap) {
-        Log.d(TAG, "setPixelMapTo: called with x${value.height}y${value.width}")
         if (pixelMap.value != null) return
         pixelMap.value = value
     }
@@ -45,27 +45,47 @@ constructor(
 
     private fun loadTheImage(pixelMap: PixelMap) {
         viewModelScope.launch(Dispatchers.IO) {
-            val result = AnnotatedString.Builder()
+            val timeInSeconds = System.currentTimeMillis().div(1000.toDouble())
+            val result = StringBuilder(pixelMap.height + (pixelMap.height * pixelMap.width))
             for (i in 0 until pixelMap.height) {
                 for (j in 0 until pixelMap.width) {
                     val color = pixelMap[j, i]
-                    result.pushStyle(
-                        SpanStyle(
-                            color = color,
-                        )
-                    )
-                    result.append("@")
-                    result.pop()
+                    val grayScaleColor = color.toGrayScale()
+                    val charPosition = grayScaleColor.times(ASCII_CHARACTERS.size.minus(1)).toInt()
+                    result.append(ASCII_CHARACTERS[charPosition])
                 }
-                result.append("\n")
-            }
-            withContext(
-                Dispatchers.Main
-            ) {
-                textViewResult = result.toAnnotatedString()
+                Log.d(
+                    TAG,
+                    "loadTheImage: it took ${
+                        ((System.currentTimeMillis()
+                            .div(1000.toDouble())) - timeInSeconds).format(2)
+                    } seconds to load the $i line!"
+                )
+                result.append('\n')
+                withContext(
+                    Dispatchers.Main
+                ) {
+                    textViewResult = result.toString()
+                }
             }
 
+
         }
+    }
+
+    private fun Double.format(digits: Int): String = "%5.${digits}f".format(this)
+
+    /**
+     * Convert color to gray scale.
+     *  source: https://stackoverflow.com/q/596216/10362460
+     */
+    private fun Color.toGrayScale(): Float {
+        return ((0.3f * this.red) + (0.59f * this.green) + (0.11f * this.blue))
+    }
+
+    companion object {
+        private val ASCII_CHARACTERS =
+            ( "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ").toCharArray()
     }
 
 
